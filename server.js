daa95f5e588ef0f33920fe9ec53d40055e202281
limunosekai <powerlsh0103@gmail.com>
2021-06-12 13:09:36 +0900

    Init

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
