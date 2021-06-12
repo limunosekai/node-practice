bab76e70e06d67595a243910caef194b6939b8c1
limunosekai <powerlsh0103@gmail.com>
2021-06-12 14:06:05 +0900

    Add client && nodemon && concurrently

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ const app = express();
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
+
 module.exports = app;
